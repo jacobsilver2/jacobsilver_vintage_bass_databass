82cbb3f76f2b5c2b9e596687923fbb85ad84f9b3
jacobsilver2 <jacobsilver2@mac.com>
2018-02-05 18:30:36 -0500

    altered dependancy flow based on Avi's video

--- lib/jacobsilver_vintage_bass_databass.rb
+++ lib/jacobsilver_vintage_bass_databass.rb
@@ -1,5 +1,3 @@
-require "jacobsilver_vintage_bass_databass/version"
+require_relative "./jacobsilver_vintage_bass_databass/version"
+require_relative "./jacobsilver_vintage_bass_databass/vintage_bass_controller"
 
-module JacobsilverVintageBassDatabass
-  # Your code goes here...
-end
